1afc74a0c140ccdae09146ee4cb6adff47730d32
borim <skqhfla@naver.com>
2025-03-26 08:22:34 +0000

    delete double

--- openssl/crypto/evp/evp_enc.c
+++ openssl/crypto/evp/evp_enc.c
@@ -17,12 +17,6 @@
 #include "internal/evp_int.h"
 #include "evp_locl.h"
 
-int jinho_aes_gcm_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
-                          const unsigned char *in, size_t len, unsigned char *keystream);
-int borim_aes_gcm_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
-                          const unsigned char *in, size_t len, unsigned char *keystream);
-
-
 int EVP_CIPHER_CTX_reset(EVP_CIPHER_CTX *c)
 {
     if (c == NULL)
